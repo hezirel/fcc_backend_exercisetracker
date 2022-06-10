fa532716049ab36e1dbe7b945ed80f3302399015
hezirel <80560775+hezirel@users.noreply.github.com>
2022-06-10 21:25:50 +0200

    WIP Logger

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const parser = require("body-parser");
 
 app.use(cors())
 app.use(express.static('public'))
-app.use(parser.urlencoded({extended: false}))
+app.use(parser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+app.use("/api/users", (req, res, next) => {
+  console.log(`${req.method} ${req.path} ${JSON.stringify(req.body)}`);
+  next();
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
@@ -24,12 +29,15 @@ app.route("/api/users")
     const username = req.body.username;
     addUser(username, (err, user) => {
       if (err) res.send(err);
-      else res.send(user);
+      else res.json({
+        username: user.username,
+        _id: user._id
+      })
     });
   })
   .get((req, res) => {
     listUser((err, users) => {
       if (err) res.send(err);
-      else res.json(users);
+      else res.send(users);
     });
   });
