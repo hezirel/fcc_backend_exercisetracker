e11bea86880313eae9b5ddd875b50b86ceee0c2e
hezirel <80560775+hezirel@users.noreply.github.com>
2022-06-10 21:19:10 +0200

    WIP DB Setup

---
+++ db.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    username: { type: String, required: true },
+    logs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Logs' }],
+});
+
+const logSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+const createUser = (username, done) => {
+    const newUser = new User({ username });
+
+    newUser.save((err, savedUser) => {
+        if (err) return done(err);
+        else done(null, savedUser);
+    });
+};
+
+const listUser = (done) => {
+    User.find({}, (err, users) => {
+        if (err) return done(err);
+        else done(null, users);
+    });
+}
+
+exports.createUser = createUser;
+exports.listUser = listUser;
--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const parser = require("body-parser");
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(parser.urlencoded({extended: false}))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+const addUser = require("./db.js").createUser;
+const listUser = require("./db.js").listUser;
+
+app.route("/api/users")
+  .post((req, res) => {
+    const username = req.body.username;
+    addUser(username, (err, user) => {
+      if (err) res.send(err);
+      else res.send(user);
+    });
+  })
+  .get((req, res) => {
+    listUser((err, users) => {
+      if (err) res.send(err);
+      else res.json(users);
+    });
+  });
